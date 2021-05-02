2de1f57d400d85dab56fc1a2bdc75d6d554bfe71
cmjagtap <cmjagtap1@gmail.com>
2021-05-02 01:29:57 -0400

    Added explorer docker script and org1-config file

---
+++ explorer/setupExplorer.sh
@@ -0,0 +1,9 @@
+
+rm -rf dockerConfig/crypto-config
+
+mkdir -p dockerConfig/crypto-config
+
+cp -r  ../orderer/crypto-config-ca/ordererOrganizations/ dockerConfig/crypto-config/
+cp -r ../org1/crypto-config-ca/peerOrganizations/ dockerConfig/crypto-config/
+cp -r ../org2/crypto-config-ca/peerOrganizations/ dockerConfig/crypto-config/
+
