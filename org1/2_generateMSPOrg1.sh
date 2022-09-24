fa8c80d91c7ba19332936ab215184d5660f3233c
cmjagtap <cmjagtap1@gmail.com>
2022-09-24 18:21:13 +0530

    Added missing nodeOUs for user1 in org1 and org2

--- org1/2_generateMSPOrg1.sh
+++ org1/2_generateMSPOrg1.sh
@@ -69,7 +69,7 @@ generateUserMSP() {
   echo
   fabric-ca-client enroll -u https://user1:user1pw@localhost:7054 --caname ca.org1.example.com -M ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp --tls.certfiles ${PWD}/fabric-ca/org1/tls-cert.pem
 
-  mkdir -p crypto-config-ca/peerOrganizations/org1.example.com/users/Admin@org1.example.com
+	cp ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/msp/config.yaml ${PWD}/crypto-config-ca/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp
 
 }
 generateAdminMSP() {
--- org2/2_generateMSPOrg2.sh
+++ org2/2_generateMSPOrg2.sh
@@ -68,7 +68,7 @@ generateUserMSP() {
   fabric-ca-client enroll -u https://user1:user1pw@localhost:8054 --caname ca.org2.example.com -M ${PWD}/crypto-config-ca/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp --tls.certfiles ${PWD}/fabric-ca/org2/tls-cert.pem
   sleep 2
 
-  mkdir -p crypto-config-ca/peerOrganizations/org2.example.com/users/Admin@org2.example.com
+	cp ${PWD}/crypto-config-ca/peerOrganizations/org2.example.com/msp/config.yaml ${PWD}/crypto-config-ca/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/config.yaml
 
 }
 generateAdminMSP() {
